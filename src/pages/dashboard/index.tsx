c9851707521149ac5ac25171e7f5c19961de18a3
SergioLU18 <slopezurzaiz@gmail.com>
2025-01-19 03:19:57 -0600

    adds initial modal functionality

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
+import { ExpenseModal } from '../../components/expenseModal';
 
 export const Dashboard: React.FC = () => {
 
+    const [modalOpen, setModalOpen] = React.useState(false);
+
+    const handleModalSubmit = () => {
+        //TODO: Add logic to add new expense
+    }
+
     return (
         <div>
-            This is where the dashboard goes :D
+            <ExpenseModal open={modalOpen} handleSubmit={handleModalSubmit} onClose={() => setModalOpen(false)}/>
+            <button onClick={() => setModalOpen(true)}>
+                Open Modal
+            </button>
         </div>
     )
 }
